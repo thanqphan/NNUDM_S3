4aceed748f88e6637604bb5bf6fe1927234fb8fe
Thang Phan <phanthq2k2@gmail.com>
2024-04-02 14:29:18 +0700

    init

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  SECRET_KEY: "TUNG123",
+  EXPIRE_JWT: "1d",
+  hostName: "localhost:3000",
+  Host: "sandbox.smtp.mailtrap.io",
+  Port: " 25",
+  Username: "502c939bb18768",
+  Password: "ecd576c83cc1fd",
+};
